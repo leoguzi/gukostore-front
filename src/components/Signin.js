7d0b146f5453bf21edba06dc833e92aac5a73083
Bruno Kohn <brunokohn1993@gmail.com>
2021-11-10 16:30:24 -0300

    heroku route implemented

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,62 +1,68 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom"; //useHistory
-import { useState } from "react"; //useContext
-//import axios from "axios";
-//import UserContext from "../contexts/UserContext.js";
+import styled from 'styled-components';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import UserContext from '../contexts/UserContext.js';
+import logo from '../images/gukologo.png';
 
 export default function Signup() {
-  //const history = useHistory();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [clicked, setClicked] = useState(false);
-  //const { setUserData } = useContext(UserContext);
+  const { setUserData } = useContext(UserContext);
 
   function toLogin(event) {
     event.preventDefault();
     setClicked(true);
-    //const body = { email, password };
+    const body = { email, password };
 
-    //const req = axios.post(`COLOCAR A URL AQUI`, body);
+    const req = axios.post(`https://gukostore.herokuapp.com/signin`, body);
 
-    /*req.then((resp) => {
+    req.then((resp) => {
       setUserData(resp.data);
-      localStorage.setItem("loginUser", JSON.stringify(resp.data));
-      history.pushState("/");
+      localStorage.setItem('loginUser', JSON.stringify(resp.data));
+      navigate('/');
     });
 
     req.catch((error) => {
-      setEmail("");
-      setPassword("");
+      setEmail('');
+      setPassword('');
       setClicked(false);
-      alert("alertar algo");
-    });*/
+      alert('Oh no! Something went wrong. Please try again');
+    });
   }
 
   return (
     <Container>
       <Background></Background>
+      <Header>
+        <StyledLink to='/'>
+          <img src={logo} alt='logo' />
+        </StyledLink>
+      </Header>
       <Holder>
         <Login>Login</Login>
         <ContainerForm>
           <form onSubmit={toLogin}>
-            <Email
-              type="email"
-              placeholder="E-mail"
+            <FormField
+              type='email'
+              placeholder='E-mail'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               disabled={clicked}
             />
-            <Password
-              type="password"
-              placeholder="Password"
+            <FormField
+              type='password'
+              placeholder='Password'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               disabled={clicked}
             />
-            <LoginButton type="submit">Login</LoginButton>
+            <LoginButton type='submit'>Login</LoginButton>
           </form>
         </ContainerForm>
-        <Link to="/signup">
+        <Link to='/signup'>
           <RegisterRedirection>Are you new here?</RegisterRedirection>
         </Link>
       </Holder>
@@ -71,13 +77,34 @@ const Container = styled.div`
   width: 100vw;
 `;
 
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  img {
+    height: 100%;
+  }
+`;
+
 const Background = styled.div`
   height: 100vh;
   width: 100vw;
-  background-image: url("https://images.pexels.com/photos/65718/pexels-photo-65718.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+  background-image: url('https://images.pexels.com/photos/65718/pexels-photo-65718.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
   background-repeat: no-repeat;
   background-size: cover;
   opacity: 0.4;
+`;
+
+const Header = styled.div`
+  height: 70px;
+  width: 100vw;
+  background-color: rgba(255, 255, 255, 0.7);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
 `;
 
 const Holder = styled.div`
@@ -94,7 +121,7 @@ const Login = styled.h1`
   line-height: 50px;
   text-align: center;
   margin-bottom: 25px;
-  font-family: "Russo One", sans-serif;
+  font-family: 'Russo One', sans-serif;
 `;
 
 const ContainerForm = styled.div`
@@ -105,35 +132,18 @@ const ContainerForm = styled.div`
   text-align: center;
 `;
 
-const Email = styled.input`
+const FormField = styled.input`
   width: 326px;
   height: 58px;
   border-radius: 5px;
   border: none;
   margin-bottom: 15px;
   padding-left: 15px;
-  font-family: "Cuprum", sans-serif;
+  font-family: 'Cuprum', sans-serif;
   font-size: 20px;
   background-color: #e2e2e2;
   ::placeholder {
-    font-family: "Cuprum", sans-serif;
-    font-size: 20px;
-    color: #000000;
-  }
-`;
-
-const Password = styled.input`
-  width: 326px;
-  height: 58px;
-  border-radius: 5px;
-  border: none;
-  margin-bottom: 15px;
-  padding-left: 15px;
-  font-family: "Cuprum", sans-serif;
-  font-size: 20px;
-  background-color: #e2e2e2;
-  ::placeholder {
-    font-family: "Cuprum", sans-serif;
+    font-family: 'Cuprum', sans-serif;
     font-size: 20px;
     color: #000000;
   }
@@ -149,14 +159,14 @@ const LoginButton = styled.button`
   color: #ffffff;
   font-weight: bold;
   font-size: 20px;
-  font-family: "Cuprum", sans-serif;
+  font-family: 'Cuprum', sans-serif;
   margin-bottom: 30px;
 `;
 
 const RegisterRedirection = styled.h2`
   font-weight: bold;
   font-size: 18px;
-  font-family: "Cuprum", sans-serif;
+  font-family: 'Cuprum', sans-serif;
   line-height: 18px;
   color: #000000;
   text-align: center;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,87 +1,93 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-//import axios from "axios";
+import styled from 'styled-components';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import logo from '../images/gukologo.png';
 
 export default function Signup() {
-  //const history = useHistory();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const [clicked, setClicked] = useState(false);
 
   function toRegister(event) {
     event.preventDefault();
     setClicked(true);
-    //const body = { name, email, address, password, passwordConfirmation };
+    const body = { name, email, address, password, passwordConfirmation };
 
     if (passwordConfirmation !== password) {
-      alert("Passwords do not match, please retype");
+      alert('Passwords do not match, please retype');
     } else {
-      //const req = axios.post(`COLOCAR A URL AQUI`, body);
-      //req.then((resp) => {
-      //history.push("/");
-      //});
-      //req.catch((error) => {
-      //  setName("");
-      //setEmail("");
-      //setPassword("");
-      //setPasswordConfirmation("");
-      //setClicked(false);
-      //alert("Oh no! Something went wrong. Please try again");
-      //});
+      const req = axios.post(`https://gukostore.herokuapp.com/signup`, body);
+      req.then((resp) => {
+        navigate('/signin');
+      });
+      req.catch((error) => {
+        setName('');
+        setEmail('');
+        setPassword('');
+        setPasswordConfirmation('');
+        setClicked(false);
+        alert('Oh no! Something went wrong. Please try again');
+      });
     }
   }
   return (
     <Container>
       <Background></Background>
+      <Header>
+        <StyledLink to='/'>
+          <img src={logo} alt='logo' />
+        </StyledLink>
+      </Header>
       <Holder>
         <Register>Register</Register>
         <ContainerForm>
           <form onSubmit={toRegister}>
-            <Name
-              type="text"
-              placeholder="Name"
+            <FormField
+              type='text'
+              placeholder='Name'
               value={name}
               onChange={(e) => setName(e.target.value)}
               disabled={clicked}
             />
-            <Email
-              type="email"
-              placeholder="E-mail"
+            <FormField
+              type='email'
+              placeholder='E-mail'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               disabled={clicked}
             />
-            <Address
-              type="text"
-              placeholder="Address"
+            <FormField
+              type='text'
+              placeholder='Address'
               value={address}
               onChange={(e) => setAddress(e.target.value)}
               disabled={clicked}
             />
-            <Password
-              type="password"
-              placeholder="Password"
+            <FormField
+              type='password'
+              placeholder='Password'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               disabled={clicked}
             />
-            <PasswordConfirmation
-              type="password"
-              placeholder="Confirm Password"
+            <FormField
+              type='password'
+              placeholder='Confirm Password'
               value={passwordConfirmation}
               onChange={(e) => setPasswordConfirmation(e.target.value)}
               disabled={clicked}
             />
-            <RegisterButton type="submit" disabled={clicked}>
+            <RegisterButton type='submit' disabled={clicked}>
               Register
             </RegisterButton>
           </form>
         </ContainerForm>
-        <Link to="/signin">
+        <Link to='/signin'>
           <LoginRedirection>Have you bought from us before?</LoginRedirection>
         </Link>
       </Holder>
@@ -97,10 +103,31 @@ const Container = styled.div`
   background-color: #fff;
 `;
 
+const Header = styled.div`
+  height: 70px;
+  width: 100vw;
+  background-color: rgba(255, 255, 255, 0.7);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  img {
+    height: 100%;
+  }
+`;
+
 const Background = styled.div`
   height: 100vh;
   width: 100vw;
-  background-image: url("https://images.pexels.com/photos/65718/pexels-photo-65718.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+  background-image: url('https:images.pexels.com/photos/65718/pexels-photo-65718.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
   background-repeat: no-repeat;
   background-size: cover;
   opacity: 0.4;
@@ -120,7 +147,7 @@ const Register = styled.h1`
   line-height: 50px;
   text-align: center;
   margin-bottom: 25px;
-  font-family: "Russo One", sans-serif;
+  font-family: 'Russo One', sans-serif;
 `;
 
 const ContainerForm = styled.div`
@@ -131,86 +158,18 @@ const ContainerForm = styled.div`
   text-align: center;
 `;
 
-const Name = styled.input`
+const FormField = styled.input`
   width: 326px;
   height: 58px;
   border-radius: 5px;
   border: none;
   margin-bottom: 15px;
   padding-left: 15px;
-  font-family: "Cuprum", sans-serif;
+  font-family: 'Cuprum', sans-serif;
   font-size: 20px;
   background-color: #e2e2e2;
   ::placeholder {
-    font-family: "Cuprum", sans-serif;
-    font-size: 20px;
-    color: #000000;
-  }
-`;
-
-const Email = styled.input`
-  width: 326px;
-  height: 58px;
-  border-radius: 5px;
-  border: none;
-  margin-bottom: 15px;
-  padding-left: 15px;
-  font-family: "Cuprum", sans-serif;
-  font-size: 20px;
-  background-color: #e2e2e2;
-  ::placeholder {
-    font-family: "Cuprum", sans-serif;
-    font-size: 20px;
-    color: #000000;
-  }
-`;
-
-const Address = styled.input`
-  width: 326px;
-  height: 58px;
-  border-radius: 5px;
-  border: none;
-  margin-bottom: 15px;
-  padding-left: 15px;
-  font-family: "Cuprum", sans-serif;
-  font-size: 20px;
-  background-color: #e2e2e2;
-  ::placeholder {
-    font-family: "Cuprum", sans-serif;
-    font-size: 20px;
-    color: #000000;
-  }
-`;
-
-const Password = styled.input`
-  width: 326px;
-  height: 58px;
-  border-radius: 5px;
-  border: none;
-  margin-bottom: 15px;
-  padding-left: 15px;
-  font-family: "Cuprum", sans-serif;
-  font-size: 20px;
-  background-color: #e2e2e2;
-  ::placeholder {
-    font-family: "Cuprum", sans-serif;
-    font-size: 20px;
-    color: #000000;
-  }
-`;
-
-const PasswordConfirmation = styled.input`
-  width: 326px;
-  height: 58px;
-  border-radius: 5px;
-  border: none;
-  margin-bottom: 15px;
-  padding-left: 15px;
-  font-family: "Cuprum", sans-serif;
-  font-size: 20px;
-  background-color: #e2e2e2;
-  ::placeholder {
-    font-family: "Cuprum", sans-serif;
+    font-family: 'Cuprum', sans-serif;
     font-size: 20px;
     color: #000000;
   }
@@ -226,14 +185,14 @@ const RegisterButton = styled.button`
   color: #ffffff;
   font-weight: bold;
   font-size: 20px;
-  font-family: "Cuprum", sans-serif;
+  font-family: 'Cuprum', sans-serif;
   margin-bottom: 30px;
 `;
 
 const LoginRedirection = styled.h2`
   font-weight: bold;
   font-size: 18px;
-  font-family: "Cuprum", sans-serif;
+  font-family: 'Cuprum', sans-serif;
   line-height: 18px;
   color: #000000;
   text-align: center;
